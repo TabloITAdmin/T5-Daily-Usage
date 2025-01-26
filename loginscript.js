2ebd59d680eac4a432ec8cf1c91579c345731d1a
TabloITAdmin <164124771+TabloITAdmin@users.noreply.github.com>
2025-01-26 17:43:04 +0800

    Add files via upload

---
+++ loginscript.js
@@ -0,0 +1,23 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "Dan" && password === "00493") {
+        window.open("main.html", "_self");
+    }
+    else if (username === "Eca" && password === "00661") {
+        window.open("main.html", "_self");
+    }
+    else if (username === "Mjp" && password === "00572") {
+        window.open("main.html", "_self");
+    }
+    else {
+        loginErrorMsg.style.opacity = 1;
+    }
+
+})
